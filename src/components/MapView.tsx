f046fa4570d4e69ced780d0341439ec45dc9e7d2
JaacielBriseo <jaacielbris23@gmail.com>
2022-12-11 01:47:59 -0700

    Show mapbox map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,14 +1,38 @@
-import { useContext } from "react"
-import { PlacesContext } from "../context"
-import { Loading } from "./"
+import { Map } from 'mapbox-gl';
+import { useContext, useLayoutEffect, useRef } from 'react';
+import { PlacesContext } from '../context';
+import { Loading } from './';
 
 export const MapView = () => {
+	const { isLoading, userlocation } = useContext(PlacesContext);
+	const mapDiv = useRef<HTMLDivElement>(null);
 
-    const {isLoading,userlocation} = useContext(PlacesContext)
-    if(isLoading){
-        return (<Loading/>)
-    }
-  return (
-    <div>{userlocation?.join(',')}</div>
-  )
-}
+	useLayoutEffect(() => {
+		if (!isLoading) {
+			const map = new Map({
+				container: mapDiv.current!, // container ID
+				style: 'mapbox://styles/mapbox/streets-v12', // style URL
+				center: userlocation, // starting position [lng, lat]
+				zoom: 9, // starting zoom
+			});
+		}
+	}, [isLoading]);
+
+	if (isLoading) {
+		return <Loading />;
+	}
+	return (
+		<div
+			ref={mapDiv}
+			style={{
+				height: '100vh',
+				width: '100vw',
+				position: 'fixed',
+				top: '0',
+				left: '0',
+			}}
+		>
+			{userlocation?.join(',')}
+		</div>
+	);
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
 
-if(!navigator.geolocation){
-	alert('Tu navegador no tiene opción de Geolocation')
-	throw new Error ('Tu navegador no tiene opción de Geolocation')
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFhY2llbGIiLCJhIjoiY2xiajN1MXpoMTV2aTNucHIwZXRmY2tsdSJ9.C2-OLQLA45m2KWfV7LHYPQ';
+if (!navigator.geolocation) {
+	alert('Tu navegador no tiene opción de Geolocation');
+	throw new Error('Tu navegador no tiene opción de Geolocation');
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
