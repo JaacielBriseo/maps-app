f4d8d3ae5008e812703e90886c50e000e21c803e
JaacielBriseo <jaacielbris23@gmail.com>
2022-12-18 20:04:57 -0700

    Some configurations for correct deploy on netlify

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+/* eslint import/no-webpack-loader-syntax:off*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
-
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+//@ts-ignore
+import mapboxgl from '!mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiamFhY2llbGIiLCJhIjoiY2xiajN1MXpoMTV2aTNucHIwZXRmY2tsdSJ9.C2-OLQLA45m2KWfV7LHYPQ';
 if (!navigator.geolocation) {
