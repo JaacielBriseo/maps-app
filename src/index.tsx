df95f5f46ed53e473e318f63629c92311facb019
JaacielBriseo <jaacielbris23@gmail.com>
2023-02-15 17:04:34 -0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { MapsApp } from './MapsApp';
 //@ts-ignore
 import mapboxgl from '!mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiamFhY2llbGIiLCJhIjoiY2xiajN1MXpoMTV2aTNucHIwZXRmY2tsdSJ9.C2-OLQLA45m2KWfV7LHYPQ';
+mapboxgl.accessToken = process.env.REACT_APP_MAP_KEY
 if (!navigator.geolocation) {
 	alert('Tu navegador no tiene opción de Geolocation');
 	throw new Error('Tu navegador no tiene opción de Geolocation');
